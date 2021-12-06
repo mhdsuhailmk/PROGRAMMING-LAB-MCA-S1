480b51ab704c68070fa238f877f75a40e5ddb165
Mhd Suhail Mk <mksuhail787@gmail.com>
2021-12-06 11:47:58 +0530

    Add files via upload

---
+++ 1.AREA OF CIRCLE/1area.py
@@ -0,0 +1,13 @@
+print("AREA OF CIRCLE")
+
+rad=input("enter the radius")
+
+res=int(rad)
+
+area=(3.14*res*res)
+peri=(2*3.14*res)
+
+print("AREA OF THE CIRCLE IS")
+
+print(area)
+print("PERIMETER IS:",peri)
