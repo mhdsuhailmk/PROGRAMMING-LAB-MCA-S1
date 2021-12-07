89c80264c616c49c51a44b98a48951000d7b3979
Mhd Suhail Mk <mksuhail787@gmail.com>
2021-12-07 23:58:44 +0530

    Add files via upload

---
+++ 10.GCD/10gcd.py
@@ -0,0 +1,10 @@
+a=int(input("ENTER THE FIRST NUMBER"))
+b=int(input("ENTER THE SECOND NUMBER"))
+if a<b:
+    numb=a
+else:
+    numb=b
+for i in range(1,num+1)
+    if((a%i==0) and (b%i==0)):
+        gcd=i
+print(gcd)
