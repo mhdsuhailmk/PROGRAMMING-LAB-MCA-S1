3c59d572875b4b9df8c3dd78dd30bd7e68237bdd
Mhd Suhail Mk <mksuhail787@gmail.com>
2022-03-05 16:50:44 +0530

    Add files via upload

---
+++ CYCLE-3/27_GRAPHICS/26.lambda.py
@@ -0,0 +1,11 @@
+from graphics import rectangle,circle
+from graphics.graphics_3d import cuboid,sphere
+
+rectangle.area()
+rectangle.perimeter()
+circle.area()
+circle.perimeter()
+cuboid.area()
+cuboid.perimeter()
+sphere.area()
+sphere.perimeter()
---
+++ CYCLE-3/27_GRAPHICS/graphics/graphics_3d/cuboid.py
@@ -0,0 +1,10 @@
+def area():
+    l1=int(input("Enter the length of cuboid:\t"))
+    w=int(input("Enter the width of cuboid:\t"))
+    h=int(input("Enter the height of cuboid:\t"))
+    print("Area of cuboid is:\t",2*((l1*w)+(l1*h)+(w*h)))
+def perimeter():
+    l1=int(input("Enter the length of cuboid:\t"))
+    w=int(input("Enter the width of cuboid:\t"))
+    h=int(input("Enter the height of cuboid:\t"))
+    print("Perimeter of cuboid is:\t",4*(l1+w+h))
